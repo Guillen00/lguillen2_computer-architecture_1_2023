0cfec2183f33f9ab513a637d041663a2303750d1
Guillen00 <47611671+Guillen00@users.noreply.github.com>
2023-05-14 10:02:50 -0600

    Prueba python algoritmo

--- procesador-pipeline/guardar_seno.py
+++ procesador-pipeline/guardar_seno.py
@@ -19,9 +19,15 @@ x = inicio
 while x <= fin:
     # Evaluamos la fórmula en el valor actual de x
     resultado = int(mi_formula(x))
-    
+    if resultado < 0:
+        binario = bin(abs(resultado) & 0xFFFFFFFF)[2:].zfill(32)
+        complemento = ''.join('0' if bit == '1' else '1' for bit in binario)
+        resultado1 = bin(int(complemento, 2) + 1)[2:].zfill(32)
+        resultadofinal = hex(int(resultado1,2))[2:].zfill(8)
+    else:
+        resultadofinal = hex(resultado)[2:].zfill(8)
     # Guardamos el resultado en el archivo de texto
-    archivo.write(str(resultado) + "\n")
+    archivo.write(str(resultadofinal) + "\n")
     
     # Incrementamos el valor de x para la siguiente iteración
     x += paso
